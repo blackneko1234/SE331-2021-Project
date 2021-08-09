efb3f9ce76d87c95c1b360cd9081679576d36ff2
Decha_L <blackneko1234@hotmail.com>
2021-08-09 21:38:36 +0700

    Finish home page UI
    Finish home page UI

--- src/App.vue
+++ src/App.vue
@@ -1,114 +1,44 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated class="glossy">
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          @click="leftDrawerOpen = !leftDrawerOpen"
-          aria-label="Menu"
-          icon="menu"
-        />
+    <div class="flex flex-end q-pa-md row justify-end q-gutter-xl">
+      <q-fab color="green-7" text-color="white" icon="menu" direction="down">
+        <router-link :to="{ name: 'About' }">
+          <q-fab-action
+            color="green-7"
+            text-color="white"
+            @click="onClick"
+            icon="information"
+          />
+          <q-tooltip transition-show="scale" transition-hide="scale">
+            About us
+          </q-tooltip>
+        </router-link>
 
-        <q-toolbar-title> Quasar App </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
-
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered class="bg-grey-2">
-      <q-list>
-        <q-item-label header>Essential Links</q-item-label>
-        <q-item clickable tag="a" target="_blank" href="https://quasar.dev">
-          <q-item-section avatar>
-            <q-icon name="school" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Docs</q-item-label>
-            <q-item-label caption>quasar.dev</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item
-          clickable
-          tag="a"
-          target="_blank"
-          href="https://github.com/quasarframework/"
-        >
-          <q-item-section avatar>
-            <q-icon name="code" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Github</q-item-label>
-            <q-item-label caption>github.com/quasarframework</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item
-          clickable
-          tag="a"
-          target="_blank"
-          href="https://chat.quasar.dev"
-        >
-          <q-item-section avatar>
-            <q-icon name="chat" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Discord Chat Channel</q-item-label>
-            <q-item-label caption>chat.quasar.dev</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item
-          clickable
-          tag="a"
-          target="_blank"
-          href="https://forum.quasar.dev"
-        >
-          <q-item-section avatar>
-            <q-icon name="forum" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Forum</q-item-label>
-            <q-item-label caption>forum.quasar.dev</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item
-          clickable
-          tag="a"
-          target="_blank"
-          href="https://twitter.com/quasarframework"
-        >
-          <q-item-section avatar>
-            <q-icon name="rss_feed" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Twitter</q-item-label>
-            <q-item-label caption>@quasarframework</q-item-label>
-          </q-item-section>
-        </q-item>
-      </q-list>
-    </q-drawer>
+        <router-link :to="{ name: 'Home' }">
+          <q-fab-action
+            color="green-7"
+            text-color="white"
+            @click="onClick"
+            icon="home"
+          />
+          <q-tooltip transition-show="scale" transition-hide="scale">
+            Home
+          </q-tooltip>
+        </router-link>
+      </q-fab>
+    </div>
 
     <q-page-container>
-      <HelloWorld />
+      <router-view />
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
-import { ref } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'LayoutDefault',
-
-  components: {
-    HelloWorld
-  },
-
-  setup() {
-    return {
-      leftDrawerOpen: ref(false)
-    }
-  }
-}
+export default {}
 </script>
+<style scoped>
+a {
+  text-decoration: none;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,87 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="text-h4 flex flex-center">
+    <b> List of people who has been vaccinated for COVID-19 </b>
+  </div>
+  <br />
+  <br />
+  <br />
+  <div class="row">
+    <div class="col-12 col-md-2"></div>
+    <div class="col-12 col-md-5 text-h6">Total people: 20</div>
+    <div class="col-12 col-md-2 flex flex-center justify-center">
+      <q-btn-dropdown
+        split
+        rounded
+        class="glossy"
+        color="primary"
+        label="Data per page: 20"
+        @click="onMainClick"
+      >
+        <q-list>
+          <q-item
+            clickable
+            v-close-popup
+            @click="onItemClick"
+            v-for="index in 20"
+            :key="index"
+          >
+            <q-item-section>
+              {{ index }}
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-btn-dropdown>
+    </div>
+
+    <div class="col-12 col-md-1 flex flex-end justify-end">
+      <q-btn-dropdown
+        split
+        rounded
+        class="glossy"
+        color="accent"
+        label="Sort by"
+        @click="onMainClick"
+      >
+        <q-list>
+          <q-item
+            clickable
+            v-close-popup
+            @click="onItemClick"
+            v-for="index in 20"
+            :key="index"
+          >
+            <q-item-section>
+              {{ index }}
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-btn-dropdown>
+    </div>
+    <div class="col-12 col-md-2"></div>
+  </div>
+  <br />
+  <q-page-container class="flex flex-center q-pa-md row q-gutter-lg">
+    <PeopleCard />
+  </q-page-container>
+  <div class="q-pa-lg flex flex-center">
+    <q-pagination v-model="current" :max="5" input />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import PeopleCard from '@/components/PeopleCard.vue'
+import { ref } from 'vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    PeopleCard
+  },
+  setup() {
+    return {
+      current: ref(1)
+    }
   }
 }
 </script>
