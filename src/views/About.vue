acd65858ec3f8340b5f9abd5c2eb9fb87a40b315
Vector2588 <natkamol082@gmail.com>
2021-08-09 23:38:04 +0700

    Finish About page
    Finish About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,66 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="q-pa-md">
+    <div class="row">
+      <div class="col"></div>
+      <div class="col-md-4 col-xs-12 flex flex-center">
+        <div class="row">
+          <q-avatar style="width: 100%; max-width: 400px; height: auto">
+            <img
+              src="https://scontent.fcnx3-1.fna.fbcdn.net/v/t31.18172-8/27164848_1591062260990007_7507937013261893414_o.jpg?_nc_cat=110&ccb=1-4&_nc_sid=09cbfe&_nc_ohc=DIV1PsBUZO0AX9ejBCc&_nc_ht=scontent.fcnx3-1.fna&oh=abbf0b3f2a3cd1b6a98a392730efe8e3&oe=61366781"
+            />
+          </q-avatar>
+        </div>
+        <div class="row">
+          <div class="col">
+            <h4 class="flex flex-center text-weight-bolder">
+              Natkamol Jongkittimaha
+            </h4>
+            <div class="q-pa-md">
+              <div class="row">
+                <div class="col"></div>
+                <div class="col-12">
+                  <div class="text-body1"><b>Student ID:</b> 622115010</div>
+                  <div class="text-body1">
+                    <b>GitHub:</b> https://github.com/vector2588
+                  </div>
+                </div>
+                <div class="col"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-4 col-xs-12 flex flex-center">
+        <div class="row">
+          <q-avatar style="width: 100%; max-width: 400px; height: auto">
+            <img
+              src="https://scontent.fcnx3-1.fna.fbcdn.net/v/t1.6435-9/169591733_3968656476506800_8679889858400063988_n.jpg?_nc_cat=102&ccb=1-4&_nc_sid=09cbfe&_nc_ohc=RxluTzFUm_wAX98kjYi&tn=QE-uKRI66BTccZ55&_nc_ht=scontent.fcnx3-1.fna&oh=36ef5ab681d465fc2af5d8bd07004d0f&oe=61361183"
+            />
+          </q-avatar>
+        </div>
+        <div class="row">
+          <div class="col">
+            <h4 class="flex flex-center text-weight-bolder">
+              Decha Laowraddecha
+            </h4>
+            <div class="q-pa-md">
+              <div class="row">
+                <div class="col"></div>
+                <div class="col-12">
+                  <div class="text-body1"><b>Student ID:</b> 622115011</div>
+                  <div class="text-body1">
+                    <b>GitHub:</b> https://github.com/blackneko1234
+                  </div>
+                </div>
+                <div class="col"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col"></div>
+    </div>
   </div>
 </template>
+
