93304b29aff2b5bd39d2c4be6c7df19b24b4750d
Vector2588 <natkamol082@gmail.com>
2021-08-10 22:33:14 +0700

    Add component
    Add component

---
+++ src/components/CommentForm.vue
@@ -0,0 +1,47 @@
+<template>
+  <form class="review-form" @submit.prevent="onSubmit">
+    <h3>Comment</h3>
+    <label for="DoctorName">Doctor name:</label>
+    <input id="DoctorName" v-model="DoctorName" />
+    <label for="DoctorSurname">Doctor surname:</label>
+    <input id="DoctorSurname" v-model="DoctorSurname" />
+    <label for="Description">Description:</label>
+    <textarea id="Description" v-model="Description"></textarea>
+
+    <input class="button" type="submit" value="submit" />
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      DoctorName: '',
+      DoctorSurname: '',
+      Description: ''
+    }
+  },
+  methods: {
+    onSubmit() {
+      if (
+        this.DoctorName === '' ||
+        this.DoctorSurname === '' ||
+        this.Description === ''
+      ) {
+        Alert('Review is incomplete. Please fill out every field.')
+        return
+      }
+      let DoctorComment = {
+        DoctorName: this.DoctorName,
+        DoctorSurname: this.DoctorSurname,
+        Description: this.Description,
+      }
+      this.$emit('review-submitted', DoctorComment)
+
+      this.DoctorName = ''
+      this.DoctorSurname = ''
+      this.Description = ''
+    }
+  }
+}
+</script>
---
+++ src/components/PeopleProfile.vue
@@ -0,0 +1,22 @@
+<template>
+    <router-link class="event-link" :to="{ name: 'PeopleProfile', params: { id: people.id } }">
+        <img src="{{ people.profilepic }}">
+        <h4>{{ people.name }} {{ people.surname }}</h4>
+        <h6>{{ people.age }}</h6>
+        <h6>{{ people.gender }}</h6>
+        <h6>{{ people.hometown }}</h6>
+        <h6>{{ people.status }}</h6>
+    </router-link>
+</template>
+
+<script>
+export default {
+  name: 'PeopleProfile',
+  props: {
+    people: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
